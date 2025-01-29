a9853d234fdb359cc4d4d16578b68f31cc1665b5
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:10:43 +0600

    multiplication

---
+++ multiplication.py
@@ -0,0 +1,14 @@
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 15:09:17
+topic:
+desc:
+"""
+
+value1 = input("enter first value:")
+value2 = input("enter second  value:")
+
+mul = int(value1) * int(value2)
+
+print("Multiplication of num1 and num2 : ", mul)
