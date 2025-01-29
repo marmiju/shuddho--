ff57fa19804b11842b734cf81cd1ec16876b76b6
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:08:30 +0600

    Addition

---
+++ addition.py
@@ -0,0 +1,15 @@
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 15:03:19
+
+topic: addition two number
+desc: Take two numbers as input and print their sum.
+"""
+
+value1 = input("enter first value:")
+value2 = input("enter second  value:")
+
+add = int(value1) + int(value2)
+
+print("Subtraction of num1 and num2 : ", add)
