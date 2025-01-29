f18079d1c2075f496223c9ed9856421192709a4f
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:38:13 +0600

    swap

---
+++ 29-1-25/swap.py
@@ -0,0 +1,27 @@
+
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 15:29:09
+topic:
+desc:
+"""
+
+
+
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+a, b = b, a
+
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
+
+
+
+
