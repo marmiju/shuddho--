46f16131a765a34e7b78b6bb6c7840030a069efd
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:20:58 +0600

    reminder

---
+++ 29-1-25/reminder.py
@@ -0,0 +1,15 @@
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 15:18:45
+topic:Find the Remainder
+desc:
+"""
+
+
+value1 = input("enter first value:")
+value2 = input("enter second  value:")
+
+rem = int(value1) % int(value2)
+
+print("reminder of num1 and num2 : ", rem)
