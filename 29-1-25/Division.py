7fe08f4393fd5245deea54a4c6c0c13b24386a42
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:12:43 +0600

    division

---
+++ 29-1-25/Division.py
@@ -0,0 +1,14 @@
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 15:11:48
+topic: division
+desc:
+"""
+
+value1 = input("enter first value:")
+value2 = input("enter second  value:")
+
+div = int(value1) / int(value2)
+
+print("division of num1 and num2 : ", div)
