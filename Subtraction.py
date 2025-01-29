cdb769f875e896f2bd9311f9e856024dcd03b93c
MAR miju <mar.miju.dev@gmail.com>
2025-01-29 15:02:09 +0600

    substraction

---
+++ Subtraction.py
@@ -0,0 +1,17 @@
+"""
+Author: Azizar Rahman
+Position: Software Developer at Inovate Digital
+Date: 2025-01-29 14:53:31
+
+topic:Subtraction of Two Numbers
+desc:Take two numbers as input and print their difference.
+"""
+
+
+value1 = input("enter first value:")
+value2 = input("enter second  value:")
+
+sub = int(value1) - int(value2)
+
+print("Subtraction of num1 and num2 : ", sub)
+
--- print.py
+++ print.py
@@ -1,8 +1,12 @@
 """
 Author: Azizar Rahman
 Position: Software Developer at Inovate Digital
-Date: 2025-01-29 14:44:20
+Date: 2025-01-29 14:52:29
+topic: print hello world!
+desc:
 """
 
 
-print("hello world")
+
+
+print("hello world!")
